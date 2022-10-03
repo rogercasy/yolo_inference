0787630c4f6c7d11c62d2e58598866457990b282
ubuntu2204 <ubuntu2204@ubuntu22.04VM>
2022-10-03 12:02:26 +0200

    Modifying README

--- conv_weights2onnx.py
+++ conv_weights2onnx.py
@@ -10,7 +10,7 @@ from tool.utils import *
 from tool.darknet2onnx import *
 
 
-def main(cfg_file, namesfile, weight_file, image_path, batch_size):
+def main(cfg_file, namesfile, weight_file, batch_size):
     if batch_size <= 0:
         onnx_path_demo = transform_to_onnx(cfg_file, weight_file, batch_size)
     else:
@@ -21,13 +21,12 @@ def main(cfg_file, namesfile, weight_file, image_path, batch_size):
 
 if __name__ == '__main__':
     print("Converting to onnx ....")
-    if len(sys.argv) == 6:
+    if len(sys.argv) == 5:
         cfg_file = sys.argv[1]
         namesfile = sys.argv[2]
         weight_file = sys.argv[3]
-        image_path = sys.argv[4]
-        batch_size = int(sys.argv[5])
-        main(cfg_file, namesfile, weight_file, image_path, batch_size)
+        batch_size = int(sys.argv[4])        
+        main(cfg_file, namesfile, weight_file, batch_size)
     else:
         print('Please run this way:\n')
         print('  python conv_weights2onnx.py <cfgFile> <namesFile> <weightFile> <imageFile> <batchSize>')
